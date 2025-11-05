55d24571ed3091a109e633c442464f47ab264a6e
Iñaki <iaguvel@upv.edu.es>
2025-11-05 23:02:25 +0100

    Mejorada estructura, login + deportes + detalles + resumen

---
+++ SportsTrack/src/main/java/com/mycompany/sportstrack/LocaleBean.java
@@ -0,0 +1,27 @@
+package com.mycompany.sportstrack;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "localeBean")
+@SessionScoped
+public class LocaleBean implements Serializable {
+
+    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+}
--- SportsTrack/src/main/java/com/mycompany/sportstrack/LogoutBean.java
+++ SportsTrack/src/main/java/com/mycompany/sportstrack/LogoutBean.java
@@ -1,28 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.sportstrack;
-import java.io.IOException;
-import javax.enterprise.context.RequestScoped;
-import javax.faces.context.FacesContext;
-import javax.inject.Named;
 
-/**
- *
- * @author iaguvel
- */
-@Named
+import java.io.IOException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "logoutBean")
 @RequestScoped
 public class LogoutBean {
+
     public String logout() throws IOException {
-        // Cierra la sesión del usuario
         FacesContext.getCurrentInstance()
                 .getExternalContext()
                 .invalidateSession();
-
-        // Redirige a la página de bienvenida
         return "/bienvenida.xhtml?faces-redirect=true";
     }
 }
