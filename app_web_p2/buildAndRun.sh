234fa6f76d91d56859e619cbe967cd6cd9d786d1
Iñaki <iaguvel@upv.edu.es>
2025-11-05 11:59:20 +0100

    Eliminado proyecto para crear de nuevo

--- app_web_p2/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.mycompany/app_web_p2 .
-docker rm -f app_web_p2 || true && docker run -d -p 9080:9080 -p 9443:9443 --name app_web_p2 com.mycompany/app_web_p2
